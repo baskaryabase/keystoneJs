8a216813b749008c35ac52290f39b028f2681d5d
Unknown <baymax1298@gmail.com>
2018-05-10 11:55:41 +0530

    changed model

--- models/User.js
+++ models/User.js
@@ -5,12 +5,21 @@ var Types = keystone.Field.Types;
  * User Model
  * ==========
  */
-var User = new keystone.List('User');
+// var User = new keystone.List('User');
+var User = new keystone.List('User', {
+  map: { name: 'fullname' }
+});
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
+	email: { type: String, unique: true, lowercase: true },
+	started: { type: Boolean, default: false },
+	fullname: {type:String,index:true},
+	contactnumber: {type:Number,index:true},
+	fathersname: {type:String,index:true},
+	groupin12th: {type:String,index:true},
+	marks: {type:Number,index:true}
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
@@ -24,5 +33,5 @@ User.schema.virtual('canAccessKeystone').get(function () {
 /**
  * Registration
  */
-User.defaultColumns = 'name, email, isAdmin';
+User.defaultColumns = 'fullname, email, started, contactnumber, marks';
 User.register();
